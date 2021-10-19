5acdd8f9cf10179c6dc548deeab2fe53aadaa16d
devendra456 <devendramaurya456@gmail.com>
2021-10-19 09:40:36 +0530

    current location done

--- app/src/main/java/com/skyview/directiondemo/MapsActivity.java
+++ app/src/main/java/com/skyview/directiondemo/MapsActivity.java
@@ -1,30 +1,50 @@
 package com.skyview.directiondemo;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.Task;
 import com.skyview.directiondemo.databinding.ActivityMapsBinding;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
     private final static int LOCATION_REQUEST_CODE = 23;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationCallback locationCallback;
+    private MarkerOptions markerOptions;
+    private Marker marker;
+    private LatLng currentLatLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,24 +52,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        initViews();
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        Objects.requireNonNull(mapFragment).getMapAsync(this);
 
         requestPermission();
         locationOn();
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
     }
 
+    private void initViews() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        markerOptions = new MarkerOptions();
+        initLocationCallBack();
+    }
+
+
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("You are here"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (checkLocationEnabled()) {
+
+            getCurrentLocation();
+
+            getLiveLocation();
+
+        }
     }
 
     private void requestPermission() {
@@ -64,13 +94,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void locationOn() {
         if (!checkLocationEnabled()) {
             new AlertDialog.Builder(this)
-                    .setTitle("Location")  // GPS not found
-                    .setMessage("Turn on Location") // Want to enable?
-                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-                        }
-                    })
+                    .setTitle("Location")
+                    .setMessage("Turn on Location")
+                    .setPositiveButton(android.R.string.yes, (dialogInterface, i) -> startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS)))
                     .setNegativeButton(android.R.string.no, null)
                     .show();
         }
@@ -85,4 +111,79 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         return false;
     }
 
+
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        if (checkLocationEnabled()) {
+
+            Task<Location> location = fusedLocationProviderClient.getLastLocation();
+            location.addOnSuccessListener((Location location1) -> {
+                marker = mMap.addMarker(markerOptions
+                        .position(new LatLng(location1.getLatitude(), location1.getLongitude())));
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(
+                        new LatLng(location1.getLatitude(), location1.getLongitude()), 16f);
+                mMap.animateCamera(cameraUpdate);
+            });
+        }
+    }
+
+    private void initLocationCallBack() {
+        //System.out.println("called===============================318");
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull @NotNull LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                //System.out.println("called===============================325");
+                Location location = locationResult.getLastLocation();
+                if (location != null) {
+                    if (marker != null) {
+                        marker.remove();
+                    }
+                    currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    marker = mMap.addMarker(markerOptions
+                            .position(currentLatLng));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng));
+                }
+            }
+
+            @Override
+            public void onLocationAvailability(@NonNull @NotNull LocationAvailability locationAvailability) {
+                super.onLocationAvailability(locationAvailability);
+            }
+        };
+    }
+
+    private void getLiveLocation() {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        locationRequest.setInterval(3000);
+        locationRequest.setFastestInterval(1000);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, getMainLooper());
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case LOCATION_REQUEST_CODE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (checkLocationEnabled()) {
+                        getCurrentLocation();
+                    }
+                }
+                return;
+            }
+        }
+    }
 }
